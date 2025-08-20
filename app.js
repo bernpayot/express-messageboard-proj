07abdef9e186a488a11fcc494c68b2d88b77c496
bernpayot <payotao@students.nu-dasma.edu.ph>
2025-08-20 11:21:37 +0800

    fix: changed port value

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("node:path");
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+const PORT = 10000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs")
